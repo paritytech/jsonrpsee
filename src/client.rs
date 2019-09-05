c86265b00b5996ce55bcad7369541a2b27500b46
Pierre Krieger <pierre.krieger1708@gmail.com>
2019-09-05 12:28:13 +0200

    More work on client

--- src/client.rs
+++ src/client.rs
@@ -1,13 +1,58 @@
-pub use crate::raw_client::RawClientRef;
+pub use crate::{raw_client::RawClientRef, types};
+use derive_more::*;
+use err_derive::*;
+use serde::de::DeserializeOwned;
+use std::sync::atomic::{AtomicU64, Ordering};
 
 /// Wraps around a "raw client" and analyzes everything correctly.
 pub struct Client<R> {
     inner: R,
+    /// Id to assign to the next request.
+    next_request_id: AtomicU64,
 }
 
 impl<R> Client<R>
 where
     for<'r> &'r R: RawClientRef<'r>,
 {
+    /// Starts a request.
+    pub async fn request<'a>(&'a self, method: impl Into<String>)
+        -> Result<impl DeserializeOwned, ClientError<<&'a R as RawClientRef<'a>>::Error>> {
+        let id = {
+            let i = self.next_request_id.fetch_add(1, Ordering::Relaxed);
+            if i == u64::max_value() {
+                log::error!("Overflow in client request ID assignment");
+            }
+            types::Id::Num(i)
+        };
 
+        let request = types::Request::Single(types::Call::MethodCall(types::MethodCall {
+            jsonrpc: types::Version::V2,
+            method: method.into(),
+            params: types::Params::None/*::Map(
+                Default::default()      // TODO:
+            )*/,
+            id,
+        }));
+
+        let result = self.inner.request("http://localhost:9933", request).await
+            .map_err(ClientError::Inner)?;
+
+        let val = match result {
+            types::Response::Single(types::Output::Success(s)) => s,
+            _ => panic!("error in request")       // TODO: no
+        };
+
+        Ok(types::from_value(val.result).map_err(ClientError::Deserialize)?)
+    }
+}
+
+/// Error that can happen during a request.
+#[derive(Debug)]        // TODO: derive Error
+pub enum ClientError<E> {
+    //#[error(display = "error in the raw client")]
+    Inner(/*#[error(cause)]*/ E),
+
+    //#[error(display = "error while deserializing the server response")]
+    Deserialize(serde_json::Error),
 }
