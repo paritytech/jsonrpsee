c3f113d1dec278aac2a565000c8d02c6e494b994
Pierre Krieger <pierre.krieger1708@gmail.com>
2019-09-18 12:36:39 +0200

    Some procedural macros cleanups

--- proc-macros/src/api_def.rs
+++ proc-macros/src/api_def.rs
@@ -1,12 +1,39 @@
 //! Contains implementation of the `syn::parse::Parse` trait. Allows parsing the input tokens
 //! stream in a structured way.
 
+use syn::spanned::Spanned as _;
+
 /// Multiple `ApiDefinition`s grouped into one struct.
 ///
 /// Represents the entire content of the procedural macro.
 #[derive(Debug)]
 pub struct ApiDefinitions {
     pub apis: Vec<ApiDefinition>,
+}
+
+/// A single API defined by the user.
+#[derive(Debug)]
+pub struct ApiDefinition {
+    /// Visibility of the definition (e.g. `pub`, `pub(crate)`, ...).
+    pub visibility: syn::Visibility,
+    /// Name of the API. For example `System`.
+    pub name: syn::Ident,
+    /// List of RPC functions defined for this API.
+    pub definitions: Vec<ApiMethod>,
+}
+
+/// A single JSON-RPC method definition.
+#[derive(Debug)]
+pub struct ApiMethod {
+    /// Signature of the method.
+    pub signature: syn::Signature,
+}
+
+/// Implementation detail of `ApiDefinition`.
+/// Parses one single block of function definitions.
+#[derive(Debug)]
+struct ApiMethods {
+    definitions: Vec<ApiMethod>,
 }
 
 impl syn::parse::Parse for ApiDefinitions {
@@ -19,17 +46,6 @@ impl syn::parse::Parse for ApiDefinitions {
 
         Ok(out)
     }
-}
-
-/// A single API defined by the user.
-#[derive(Debug)]
-pub struct ApiDefinition {
-    /// Visibility of the definition (e.g. `pub`, `pub(crate)`, ...).
-    pub visibility: syn::Visibility,
-    /// Name of the API. For example `System`.
-    pub name: syn::Ident,
-    /// List of RPC functions defined for this API.
-    pub definitions: Vec<syn::Signature>,
 }
 
 impl syn::parse::Parse for ApiDefinition {
@@ -48,11 +64,17 @@ impl syn::parse::Parse for ApiDefinition {
     }
 }
 
-/// Implementation detail of `ApiDefinition`.
-/// Parses one single block of function definitions.
-#[derive(Debug)]
-struct ApiMethods {
-    definitions: Vec<syn::Signature>,
+impl syn::parse::Parse for ApiMethod {
+    fn parse(input: syn::parse::ParseStream) -> syn::parse::Result<Self> {
+        let item: syn::TraitItemMethod = input.parse()?;
+        if item.default.is_some() {
+            return Err(syn::Error::new(item.default.span(),
+                "It is forbidden to provide a default implementation for methods in the API definition"));
+        }
+        Ok(ApiMethod {
+            signature: item.sig,
+        })
+    }
 }
 
 impl syn::parse::Parse for ApiMethods {
@@ -62,18 +84,8 @@ impl syn::parse::Parse for ApiMethods {
         };
 
         while !input.is_empty() {
-            let def: syn::TraitItemMethod = input.parse()?;
-            if def.default.is_some() {
-                panic!(
-                    "It is forbidden to provide a default implementation for methods in the \
-                     API definition: {:?}",
-                    def
-                );
-            }
-
-            // TODO: do something with the function attributes?
-
-            out.definitions.push(def.sig);
+            let method: ApiMethod = input.parse()?;
+            out.definitions.push(method);
         }
 
         Ok(out)
