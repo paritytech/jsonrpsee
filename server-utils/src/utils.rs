95ab173027de15f3a53de206d61e9cb4b76dfb35
dpmkl <524935+dpmkl@users.noreply.github.com>
2019-11-18 11:16:13 +0100

    Access control (#31)
    * Add initial acl implementation
    
    * Implemented access control with utilities from paritytech::jsonrpc
    
    * Incorrect if
    
    * Make allowed_hosts an enum
    
    * Handling CORS in process_request
    
    * Build for AccessControl and naming coherence
    
    * "clean up your room! clean up your room ! that's all i ever hear..."
    - preflight requests
    - test and bench
    - wasm for yew
    
    * Add basic acl tests for 'host'
    
    * Remove 'macro_use'
    
    * Apply rustfmt to server-utils
    
    * Apply rustfmt to http
    
    * Bump lazy_static dependency

---
+++ server-utils/src/utils.rs
@@ -0,0 +1,7 @@
+//! Utility methods relying on hyper
+use hyper;
+
+/// Extracts string value of a single header in request.
+pub fn read_header<'a>(req: &'a hyper::Request<hyper::Body>, header_name: &str) -> Option<&'a str> {
+    req.headers().get(header_name).and_then(|v| v.to_str().ok())
+}
