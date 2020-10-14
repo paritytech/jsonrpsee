cb9738608e768b5d2ade6a623ae41adb23832e7b
Niklas <niklasadolfsson1@gmail.com>
2020-10-14 11:07:07 +0200

    [ws server]: simple subscription test.

--- src/ws/tests.rs
+++ src/ws/tests.rs
@@ -1,141 +1,11 @@
 #![cfg(test)]
 
-use crate::common::{Id, JsonValue};
+use crate::client::{WsClient, WsSubscription};
+use crate::common::{Id, JsonValue, Params};
 use crate::ws::WsServer;
-use futures::channel::oneshot::{self, Sender};
-use futures::future::FutureExt;
-use futures::io::{BufReader, BufWriter};
-use futures::{pin_mut, select};
-use soketto::handshake;
+use futures::channel::oneshot;
+use helpers::*;
 use std::net::SocketAddr;
-use tokio::net::TcpStream;
-use tokio_util::compat::{Compat, Tokio02AsyncReadCompatExt};
-
-type Error = Box<dyn std::error::Error>;
-
-struct WebSocketTestClient {
-    tx: soketto::Sender<BufReader<BufWriter<Compat<TcpStream>>>>,
-    rx: soketto::Receiver<BufReader<BufWriter<Compat<TcpStream>>>>,
-}
-
-impl WebSocketTestClient {
-    async fn new(url: SocketAddr) -> Result<Self, Error> {
-        let socket = TcpStream::connect(url).await?;
-        let mut client = handshake::Client::new(
-            BufReader::new(BufWriter::new(socket.compat())),
-            "test-client",
-            "/",
-        );
-        match client.handshake().await {
-            Ok(handshake::ServerResponse::Accepted { .. }) => {
-                let (tx, rx) = client.into_builder().finish();
-                Ok(Self { tx, rx })
-            }
-            r @ _ => Err(format!("WebSocketHandshake failed: {:?}", r).into()),
-        }
-    }
-
-    async fn send_request_text(&mut self, msg: impl AsRef<str>) -> Result<String, Error> {
-        self.tx.send_text(msg).await?;
-        self.tx.flush().await?;
-        let mut data = Vec::new();
-        self.rx.receive_data(&mut data).await?;
-        String::from_utf8(data).map_err(Into::into)
-    }
-
-    async fn send_request_binary(&mut self, msg: &[u8]) -> Result<String, Error> {
-        self.tx.send_binary(msg).await?;
-        self.tx.flush().await?;
-        let mut data = Vec::new();
-        self.rx.receive_data(&mut data).await?;
-        String::from_utf8(data).map_err(Into::into)
-    }
-
-    async fn close(&mut self) -> Result<(), Error> {
-        self.tx.close().await.map_err(Into::into)
-    }
-}
-
-/// Spawn a WebSocket server where the OS assigns the port number and
-/// sends back the `local_addr` via a channel.
-async fn server(server_started_tx: Sender<SocketAddr>) {
-    let server = WsServer::new("127.0.0.1:0").await.unwrap();
-    let mut hello = server.register_method("say_hello".to_owned()).unwrap();
-    let mut add = server.register_method("add".to_owned()).unwrap();
-    let mut notif = server
-        .register_notification("notif".to_owned(), false)
-        .unwrap();
-    server_started_tx.send(*server.local_addr()).unwrap();
-
-    loop {
-        let hello_fut = async {
-            let handle = hello.next().await;
-            log::debug!("server respond to hello");
-            handle
-                .respond(Ok(JsonValue::String("hello".to_owned())))
-                .await;
-        }
-        .fuse();
-
-        let add_fut = async {
-            let handle = add.next().await;
-            let params: Vec<u64> = handle.params().clone().parse().unwrap();
-            let sum: u64 = params.iter().sum();
-            handle.respond(Ok(JsonValue::Number(sum.into()))).await;
-        }
-        .fuse();
-
-        let notif_fut = async {
-            let params = notif.next().await;
-            println!("received notification: say_hello params[{:?}]", params);
-        }
-        .fuse();
-
-        pin_mut!(hello_fut, add_fut, notif_fut);
-        select! {
-            say_hello = hello_fut => (),
-            add = add_fut => (),
-            notif = notif_fut => (),
-            complete => (),
-        };
-    }
-}
-
-fn ok_response(result: JsonValue, id: Id) -> String {
-    format!(
-        r#"{{"jsonrpc":"2.0","result":{},"id":{}}}"#,
-        result,
-        serde_json::to_string(&id).unwrap()
-    )
-}
-
-fn method_not_found(id: Id) -> String {
-    format!(
-        r#"{{"jsonrpc":"2.0","error":{{"code":-32601,"message":"Method not found"}},"id":{}}}"#,
-        serde_json::to_string(&id).unwrap()
-    )
-}
-
-fn parse_error(id: Id) -> String {
-    format!(
-        r#"{{"jsonrpc":"2.0","error":{{"code":-32700,"message":"Parse error"}},"id":{}}}"#,
-        serde_json::to_string(&id).unwrap()
-    )
-}
-
-fn invalid_request(id: Id) -> String {
-    format!(
-        r#"{{"jsonrpc":"2.0","error":{{"code":-32600,"message":"Invalid request"}},"id":{}}}"#,
-        serde_json::to_string(&id).unwrap()
-    )
-}
-
-fn invalid_params(id: Id) -> String {
-    format!(
-        r#"{{"jsonrpc":"2.0","error":{{"code":-32602,"message":"Invalid params"}},"id":{}}}"#,
-        serde_json::to_string(&id).unwrap()
-    )
-}
 
 #[tokio::test]
 async fn single_method_call_works() {
@@ -154,8 +24,30 @@ async fn single_method_call_works() {
     }
 }
 
+// TODO: technically more of a integration test because the "real" client is used.
 #[tokio::test]
-async fn single_method_call_with_params() {
+async fn subscription_works() {
+    let (server_started_tx, server_started_rx) = oneshot::channel::<SocketAddr>();
+    server_subscribe_only(server_started_tx);
+    let server_addr = server_started_rx.await.unwrap();
+    let uri = format!("ws://{}", server_addr);
+    let client = WsClient::new(&uri).await.unwrap();
+    let mut subscribe: WsSubscription<JsonValue> = client
+        .subscribe("subscribe_hello", Params::None, "unsubscribe_hello")
+        .await
+        .unwrap();
+
+    for _ in 0..10 {
+        let response = subscribe.next().await;
+        assert_eq!(
+            response,
+            JsonValue::String("hello from subscription".to_owned())
+        );
+    }
+}
+
+#[tokio::test]
+async fn single_method_call_with_params_works() {
     let (server_started_tx, server_started_rx) = oneshot::channel::<SocketAddr>();
     tokio::spawn(server(server_started_tx));
     let server_addr = server_started_rx.await.unwrap();
@@ -244,4 +136,162 @@ async fn register_methods_works() {
             .is_ok(),
         "Failed register_subscription should not have side-effects"
     );
+}
+
+mod helpers {
+    use crate::client::{WsClient, WsSubscription};
+    use crate::common::{Id, JsonValue, Params};
+    use crate::ws::WsServer;
+    use futures::channel::oneshot::{self, Sender};
+    use futures::future::FutureExt;
+    use futures::io::{BufReader, BufWriter};
+    use futures::{pin_mut, select};
+    use soketto::handshake;
+    use std::net::SocketAddr;
+    use tokio::net::TcpStream;
+    use tokio_util::compat::{Compat, Tokio02AsyncReadCompatExt};
+
+    type Error = Box<dyn std::error::Error>;
+
+    pub struct WebSocketTestClient {
+        tx: soketto::Sender<BufReader<BufWriter<Compat<TcpStream>>>>,
+        rx: soketto::Receiver<BufReader<BufWriter<Compat<TcpStream>>>>,
+    }
+
+    impl WebSocketTestClient {
+        pub async fn new(url: SocketAddr) -> Result<Self, Error> {
+            let socket = TcpStream::connect(url).await?;
+            let mut client = handshake::Client::new(
+                BufReader::new(BufWriter::new(socket.compat())),
+                "test-client",
+                "/",
+            );
+            match client.handshake().await {
+                Ok(handshake::ServerResponse::Accepted { .. }) => {
+                    let (tx, rx) = client.into_builder().finish();
+                    Ok(Self { tx, rx })
+                }
+                r @ _ => Err(format!("WebSocketHandshake failed: {:?}", r).into()),
+            }
+        }
+
+        pub async fn send_request_text(&mut self, msg: impl AsRef<str>) -> Result<String, Error> {
+            self.tx.send_text(msg).await?;
+            self.tx.flush().await?;
+            let mut data = Vec::new();
+            self.rx.receive_data(&mut data).await?;
+            String::from_utf8(data).map_err(Into::into)
+        }
+
+        pub async fn send_request_binary(&mut self, msg: &[u8]) -> Result<String, Error> {
+            self.tx.send_binary(msg).await?;
+            self.tx.flush().await?;
+            let mut data = Vec::new();
+            self.rx.receive_data(&mut data).await?;
+            String::from_utf8(data).map_err(Into::into)
+        }
+
+        pub async fn close(&mut self) -> Result<(), Error> {
+            self.tx.close().await.map_err(Into::into)
+        }
+    }
+
+    /// Spawns a dummy `JSONRPC v2 WebSocket` that just send subscriptions to `subscribe_hello`
+    // TODO: not sure why `tokio::spawn` works for this.
+    pub fn server_subscribe_only(server_started: Sender<SocketAddr>) {
+        std::thread::spawn(move || {
+            use async_std::task::block_on;
+            let server = block_on(WsServer::new("127.0.0.1:0")).unwrap();
+            let mut sub = server
+                .register_subscription("subscribe_hello".to_owned(), "unsubscribe_hello".to_owned())
+                .unwrap();
+            server_started.send(*server.local_addr()).unwrap();
+
+            loop {
+                block_on(sub.send(JsonValue::String("hello from subscription".to_owned())));
+                std::thread::sleep(std::time::Duration::from_millis(100));
+            }
+        });
+    }
+
+    /// Spawns a dummy `JSONRPC v2 WebSocket`
+    /// It has two hardcoded methods "say_hello" and "add", one hardcoded notification "notif"
+    pub async fn server(server_started: Sender<SocketAddr>) {
+        let server = WsServer::new("127.0.0.1:0").await.unwrap();
+        let mut hello = server.register_method("say_hello".to_owned()).unwrap();
+        let mut add = server.register_method("add".to_owned()).unwrap();
+        let mut notif = server
+            .register_notification("notif".to_owned(), false)
+            .unwrap();
+        server_started.send(*server.local_addr()).unwrap();
+
+        loop {
+            let hello_fut = async {
+                let handle = hello.next().await;
+                log::debug!("server respond to hello");
+                handle
+                    .respond(Ok(JsonValue::String("hello".to_owned())))
+                    .await;
+            }
+            .fuse();
+
+            let add_fut = async {
+                let handle = add.next().await;
+                let params: Vec<u64> = handle.params().clone().parse().unwrap();
+                let sum: u64 = params.iter().sum();
+                handle.respond(Ok(JsonValue::Number(sum.into()))).await;
+            }
+            .fuse();
+
+            let notif_fut = async {
+                let params = notif.next().await;
+                println!("received notification: say_hello params[{:?}]", params);
+            }
+            .fuse();
+
+            pin_mut!(hello_fut, add_fut, notif_fut);
+            select! {
+                say_hello = hello_fut => (),
+                add = add_fut => (),
+                notif = notif_fut => (),
+                complete => (),
+            };
+        }
+    }
+
+    pub fn ok_response(result: JsonValue, id: Id) -> String {
+        format!(
+            r#"{{"jsonrpc":"2.0","result":{},"id":{}}}"#,
+            result,
+            serde_json::to_string(&id).unwrap()
+        )
+    }
+
+    pub fn method_not_found(id: Id) -> String {
+        format!(
+            r#"{{"jsonrpc":"2.0","error":{{"code":-32601,"message":"Method not found"}},"id":{}}}"#,
+            serde_json::to_string(&id).unwrap()
+        )
+    }
+
+    pub fn parse_error(id: Id) -> String {
+        format!(
+            r#"{{"jsonrpc":"2.0","error":{{"code":-32700,"message":"Parse error"}},"id":{}}}"#,
+            serde_json::to_string(&id).unwrap()
+        )
+    }
+
+    pub fn invalid_request(id: Id) -> String {
+        format!(
+            r#"{{"jsonrpc":"2.0","error":{{"code":-32600,"message":"Invalid request"}},"id":{}}}"#,
+            serde_json::to_string(&id).unwrap()
+        )
+    }
+
+    pub fn invalid_params(id: Id) -> String {
+        format!(
+            r#"{{"jsonrpc":"2.0","error":{{"code":-32602,"message":"Invalid params"}},"id":{}}}"#,
+            serde_json::to_string(&id).unwrap()
+        )
+    }
 }
