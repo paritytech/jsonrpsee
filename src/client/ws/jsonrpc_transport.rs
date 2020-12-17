b911b78febf5331101583a2ebff98ad0e0c14894
Niklas <niklasadolfsson1@gmail.com>
2020-12-17 17:23:14 +0100

    fix(grumble): docs `JSONRPC WebSocket transport`

--- src/client/ws/jsonrpc_transport.rs
+++ src/client/ws/jsonrpc_transport.rs
@@ -1,4 +1,4 @@
-//! JSONRPC Transport module.
+//! JSONRPC WebSocket Transport module.
 //!
 //! Wraps the underlying WebSocket transport with specific JSONRPC details.
 
@@ -11,12 +11,15 @@ pub async fn websocket_connection(target: impl AsRef<str>) -> Result<(Sender, Re
 	Ok((Sender::new(sender), Receiver::new(receiver)))
 }
 
+/// JSONRPC WebSocket sender.
+/// It's a wrapper over `WebSocket sender` with additional `JSONRPC request_id`.
 pub struct Sender {
 	request_id: u64,
 	transport: crate::client::ws::transport::Sender,
 }
 
 impl Sender {
+	/// Creates a new JSONRPC sender.
 	pub fn new(transport: crate::client::ws::transport::Sender) -> Self {
 		Self { transport, request_id: 0 }
 	}
@@ -37,8 +40,8 @@ impl Sender {
 			id: jsonrpc::Id::Num(id),
 		}));
 
-		// Note that in case of an error, we "lose" the request id (as in, it will never be
-		// used). This isn't a problem, however.
+		// Note that in case of an error, we "lose" the request id.
+		// This isn't a problem, however.
 		self.transport.send_request(request).await?;
 
 		Ok(id)
@@ -46,7 +49,7 @@ impl Sender {
 
 	/// Sends a notification to the server. The notification doesn't need any response.
 	///
-	/// This asynchronous function finishes when the notification has finished being sent.
+	/// Returns `Ok(())` if the notification was successfully sent otherwise `Err(_)`.
 	pub async fn send_notification(
 		&mut self,
 		method: impl Into<String>,
@@ -61,6 +64,10 @@ impl Sender {
 		self.transport.send_request(request).await
 	}
 
+	/// Sends a request to the server but it doesn't wait for a response.
+	/// Instead, you have keep the request ID and use the [`Receiver`] to get the response.
+	///
+	/// Returns `Ok(request_id)` if the request was successfully sent otherwise `Err(_)`.
 	pub async fn start_request(
 		&mut self,
 		method: impl Into<String>,
@@ -69,6 +76,10 @@ impl Sender {
 		self.start_impl(method, params).await
 	}
 
+	/// Sends a request to the server to start a new subscription but it doesn't wait for a response.
+	/// Instead, you have keep the request ID and use the [`Receiver`] to get the response.
+	///
+	/// Returns `Ok(request_id)` if the request was successfully sent otherwise `Err(_)`.
 	pub async fn start_subscription(
 		&mut self,
 		method: impl Into<String>,
@@ -78,11 +89,13 @@ impl Sender {
 	}
 }
 
+/// JSONRPC WebSocket receiver.
 pub struct Receiver {
 	transport: crate::client::ws::transport::Receiver,
 }
 
 impl Receiver {
+	/// Create a new JSONRPC WebSocket receiver.
 	pub fn new(transport: crate::client::ws::transport::Receiver) -> Self {
 		Self { transport }
 	}
