749a67db4ea7760161d0ba2814b5efc0bac5dd2d
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2021-02-10 19:49:20 +0100

    docs(http client): document features

--- http-client/src/lib.rs
+++ http-client/src/lib.rs
@@ -1,3 +1,24 @@
+//! # jsonrpsee-http-client
+//!
+//! jsonrpsee-http-client is a small JSONRPC HTTP client built directly for `async/await`.
+//!
+//! - Swappable HTTP backends.
+//! - Supports multiple executors (depends on which backend that is configured, see [Optional features](#optional-features))
+//!
+//! # Optional Features
+//!
+//! jsonrpsee-http-client uses a set of [feature flags](https://doc.rust-lang.org/cargo/reference/manifest.html#the-features-section) to support
+//! different configuration depending in which environment you are using the client.
+//!
+//! The following optional features are available:
+//!
+//! - `hyper-client`: Enables [hyper](https://docs.rs/hyper) as HTTP transport backend which is the fastest option.
+//! This feature works only on [tokio 0.2](https://docs.rs/tokio/0.2.25/tokio/) or [async-std](https://docs.rs/async-std).
+//! - `curl-client`: Enables [curl](https://docs.rs/curl) as HTTP transport backend.
+//! - `wasm-client`: Enables JavaScript bindings as HTTP transport backend.
+//! - `middleware-logger`: Enables middleware logging from [surf](https://docs.rs/surf)
+//!
+
 mod client;
 mod transport;
 
