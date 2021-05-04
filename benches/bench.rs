78f3b25c3a02277153f2c85e9f6e1e712296faeb
David Palm <dvdplm@gmail.com>
2021-05-04 09:19:14 +0200

    Benchmark batch request over different batch sizes

--- benches/bench.rs
+++ benches/bench.rs
@@ -51,7 +51,7 @@ pub fn batched_http_requests(crit: &mut Criterion) {
 	let rt = TokioRuntime::new().unwrap();
 	let url = rt.block_on(helpers::http_server());
 	let client = Arc::new(HttpClientBuilder::default().build(&url).unwrap());
-	run_round_trip_with_batch(&rt, crit, client.clone(), "batched_http_round_trip", 10);
+	run_round_trip_with_batch(&rt, crit, client.clone(), "http batch requests");
 }
 
 pub fn websocket_requests(crit: &mut Criterion) {
@@ -73,21 +73,26 @@ fn run_round_trip(rt: &TokioRuntime, crit: &mut Criterion, client: Arc<impl Clie
 	});
 }
 
+/// Benchmark http batch requests over batch sizes of 2, 5, 10, 50 and 100 RPCs in each batch.
 fn run_round_trip_with_batch(
 	rt: &TokioRuntime,
 	crit: &mut Criterion,
 	client: Arc<impl Client>,
 	name: &str,
-	batch_size: usize,
 ) {
-	let batch = vec![("say_hello", JsonRpcParams::NoParams); batch_size];
-	crit.bench_function(name, |b| {
-		b.iter(|| {
-			rt.block_on(async {
-				black_box(client.batch_request::<String>(batch.clone()).await.unwrap());
+	let mut group = crit.benchmark_group(name);
+	for batch_size in [2, 5, 10, 50, 100usize].iter() {
+		let batch = vec![("say_hello", JsonRpcParams::NoParams); *batch_size];
+		group.throughput(Throughput::Elements(*batch_size as u64));
+		group.bench_with_input(BenchmarkId::from_parameter(batch_size), batch_size, |b, _| {
+			b.iter(|| {
+				rt.block_on( async {
+					client.batch_request::<String>(batch.clone()).await.unwrap()
+				})
 			})
-		})
-	});
+		});
+	}
+	group.finish();
 }
 
 fn run_concurrent_round_trip<C: 'static + Client + Send + Sync>(
