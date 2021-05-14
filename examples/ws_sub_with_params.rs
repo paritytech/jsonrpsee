f48770d573c5e2fb3194bfe83a6215f126e16e4b
David Palm <dvdplm@gmail.com>
2021-05-14 18:58:37 +0200

    example

---
+++ examples/ws_sub_with_params.rs
@@ -0,0 +1,68 @@
+// Copyright 2019 Parity Technologies (UK) Ltd.
+//
+// Permission is hereby granted, free of charge, to any
+// person obtaining a copy of this software and associated
+// documentation files (the "Software"), to deal in the
+// Software without restriction, including without
+// limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software
+// is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice
+// shall be included in all copies or substantial portions
+// of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
+// ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+// SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+// IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+use jsonrpsee::{
+	ws_client::{traits::SubscriptionClient, v2::params::JsonRpcParams, Subscription, WsClientBuilder},
+	ws_server::{WsServer, InnerSubSinkParams},
+};
+use std::net::SocketAddr;
+use serde_json::value::Value as JsonValue;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+	env_logger::init();
+	let addr = run_server().await?;
+	let url = format!("ws://{}", addr);
+
+	let client = WsClientBuilder::default().build(&url).await?;
+
+	let param: JsonValue = 3.into();
+	let params = JsonRpcParams::Array(vec![param]);
+	let mut sub_params = client.subscribe::<Option<char>>("sub_params", params, "unsub_params").await?;
+	println!("subscription with params: {:?}", sub_params.next().await);
+
+	Ok(())
+}
+
+async fn run_server() -> anyhow::Result<SocketAddr> {
+	let mut server = WsServer::new("127.0.0.1:0").await?;
+	let mut sub = server.register_subscription_with_params("sub_params", "unsub_params").unwrap();
+
+	std::thread::spawn(move || loop {
+		const LETTERS: &'static str = "abcdefghijklmnopqrstuvxyz";
+		sub.next().and_then(|inner_sub_sink_params: InnerSubSinkParams<usize>| {
+			let idx = *inner_sub_sink_params.params();
+			let result = LETTERS.chars().nth(idx);
+			inner_sub_sink_params.send(&result);
+			result
+		});
+		std::thread::sleep(std::time::Duration::from_millis(100));
+	});
+
+	let addr = server.local_addr();
+	tokio::spawn(async move { server.start().await });
+	addr
+}
