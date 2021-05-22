2f234a030e0c175ff3f5a9d8491bed1bf0cdc75b
Igor Aleksanov <popzxc@yandex.ru>
2021-05-22 16:13:47 +0400

    Add timeouts to the futures in tests (#332)
    * Add extension trait to timeout the futures execution
    
    * Use timeout for ws-server tests
    
    * Use timeout for ws-client tests
    
    * Use timeout for http-server tests
    
    * Use timeout for http-client tests
    
    * Make TimeoutFutureExt interafaces more explicit

--- test-utils/src/lib.rs
+++ test-utils/src/lib.rs
@@ -2,5 +2,33 @@
 
 #![recursion_limit = "256"]
 
+use std::{future::Future, time::Duration};
+use tokio::time::{timeout, Timeout};
+
 pub mod helpers;
 pub mod types;
+
+/// Helper extension trait which allows to limit execution time for the futures.
+/// It is helpful in tests to ensure that no future will ever get stuck forever.
+pub trait TimeoutFutureExt<T>: Future<Output = T> + Sized {
+	/// Returns a reasonable value that can be used as a future timeout with a certain
+	/// degree of confidence that timeout won't be triggered by the test specifics.
+	fn default_timeout() -> Duration {
+		// If some future wasn't done in 60 seconds, it's either a poorly written test
+		// or (most likely) a bug related to some future never actually being completed.
+		const TIMEOUT_SECONDS: u64 = 60;
+		Duration::from_secs(TIMEOUT_SECONDS)
+	}
+
+	/// Adds a fixed timeout to the future.
+	fn with_default_timeout(self) -> Timeout<Self> {
+		self.with_timeout(Self::default_timeout())
+	}
+
+	/// Adds a custom timeout to the future.
+	fn with_timeout(self, timeout_value: Duration) -> Timeout<Self> {
+		timeout(timeout_value, self)
+	}
+}
+
+impl<T, U> TimeoutFutureExt<T> for U where U: Future<Output = T> + Sized {}
