ab3b8c036f7dc5e8e5acc214f9ae4bd5b49518c0
Igor Aleksanov <popzxc@yandex.ru>
2021-06-17 09:03:05 +0300

    Start working on the server impl

--- proc-macros/src/new/render_server.rs
+++ proc-macros/src/new/render_server.rs
@@ -1,8 +1,43 @@
 use super::RpcDescription;
 use proc_macro2::TokenStream as TokenStream2;
+use quote::quote;
 
 impl RpcDescription {
 	pub(super) fn render_server(&self) -> Result<TokenStream2, syn::Error> {
+		let trait_name = quote::format_ident!("{}Server", &self.trait_def.ident);
+
+		let method_impls = self.render_methods()?;
+		let into_rpc_impl = self.render_into_rpc()?;
+
+		let async_trait = self.jrps_item(quote! { __reexports::async_trait });
+
+		// Doc-comment to be associated with the server.
+		let doc_comment = format!("Server trait implementation for the `{}` RPC API.", &self.trait_def.ident);
+
+		let trait_impl = quote! {
+			#[#async_trait]
+			#[doc = #doc_comment]
+			pub trait #trait_name {
+				#method_impls
+				#into_rpc_impl
+			}
+		};
+
+		Ok(trait_impl)
+	}
+
+	fn render_methods(&self) -> Result<TokenStream2, syn::Error> {
+		let methods = self.methods.iter().map(|method| &method.signature);
+		let subscriptions = self.subscriptions.iter().map(|sub| &sub.signature);
+
+		Ok(quote! {
+			#(#methods)*
+			#(#subscriptions)*
+		})
+	}
+
+	fn render_into_rpc(&self) -> Result<TokenStream2, syn::Error> {
+		// let jsonrpsee = &self.jsonrpsee_path;
 		todo!()
 	}
 }
