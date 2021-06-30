216bc65016d1edbc618310f7c3e76f7b40be2d59
Maciej Hirsz <hello@maciej.codes>
2021-06-30 18:53:22 +0200

    Set allowed Host header values

--- ws-server/src/server.rs
+++ ws-server/src/server.rs
@@ -208,7 +208,10 @@ async fn handshake(
 
 	let key = {
 		let req = server.receive_request().await?;
-		cfg.allowed_origins.verify(req.headers().origin).map(|()| req.key())
+		let host_check = cfg.allowed_hosts.verify("Host", Some(req.headers().host));
+		let origin_check = cfg.allowed_origins.verify("Origin", req.headers().origin);
+
+		host_check.and(origin_check).map(|()| req.key())
 	};
 
 	match key {
@@ -324,16 +327,16 @@ async fn background_task(
 }
 
 #[derive(Debug, Clone)]
-enum AllowedOrigins {
+enum AllowedValue {
 	Any,
-	OneOf(Arc<[String]>),
+	OneOf(Box<[String]>),
 }
 
-impl AllowedOrigins {
-	fn verify(&self, origin: Option<&[u8]>) -> Result<(), Error> {
-		if let (AllowedOrigins::OneOf(list), Some(origin)) = (self, origin) {
-			if !list.iter().any(|o| o.as_bytes() == origin) {
-				let error = format!("Origin denied: {}", String::from_utf8_lossy(origin));
+impl AllowedValue {
+	fn verify(&self, header: &str, value: Option<&[u8]>) -> Result<(), Error> {
+		if let (AllowedValue::OneOf(list), Some(value)) = (self, value) {
+			if !list.iter().any(|o| o.as_bytes() == value) {
+				let error = format!("{} denied: {}", header, String::from_utf8_lossy(value));
 				log::warn!("{}", error);
 				return Err(Error::Request(error));
 			}
@@ -350,8 +353,10 @@ struct Settings {
 	max_request_body_size: u32,
 	/// Maximum number of incoming connections allowed.
 	max_connections: u64,
-	/// Cross-origin policy by which to accept or deny incoming requests.
-	allowed_origins: AllowedOrigins,
+	/// Policy by which to accept or deny incoming requests based on the `Origin` header.
+	allowed_origins: AllowedValue,
+	/// Policy by which to accept or deny incoming requests based on the `Host` header.
+	allowed_hosts: AllowedValue,
 }
 
 impl Default for Settings {
@@ -359,7 +364,8 @@ impl Default for Settings {
 		Self {
 			max_request_body_size: TEN_MB_SIZE_BYTES,
 			max_connections: MAX_CONNECTIONS,
-			allowed_origins: AllowedOrigins::Any,
+			allowed_origins: AllowedValue::Any,
+			allowed_hosts: AllowedValue::Any,
 		}
 	}
 }
@@ -389,7 +395,7 @@ impl Builder {
 	///
 	/// ```rust
 	/// # let mut builder = jsonrpsee_ws_server::WsServerBuilder::default();
-	/// builder.set_allowed_origins(vec!["https://example.com"]);
+	/// builder.set_allowed_origins(["https://example.com"]);
 	/// ```
 	///
 	/// By default allows any `Origin`.
@@ -400,13 +406,13 @@ impl Builder {
 		List: IntoIterator<Item = Origin>,
 		Origin: Into<String>,
 	{
-		let list: Arc<_> = list.into_iter().map(Into::into).collect();
+		let list: Box<_> = list.into_iter().map(Into::into).collect();
 
 		if list.len() == 0 {
 			return Err(Error::EmptyAllowedOrigins);
 		}
 
-		self.settings.allowed_origins = AllowedOrigins::OneOf(list);
+		self.settings.allowed_origins = AllowedValue::OneOf(list);
 
 		Ok(self)
 	}
@@ -414,7 +420,31 @@ impl Builder {
 	/// Restores the default behavior of allowing connections with `Origin` header
 	/// containing any value. This will undo any list set by [`set_allowed_origins`](Builder::set_allowed_origins).
 	pub fn allow_all_origins(mut self) -> Self {
-		self.settings.allowed_origins = AllowedOrigins::Any;
+		self.settings.allowed_origins = AllowedValue::Any;
+		self
+	}
+
+	/// Set a list of allowed hosts. During the handshake, the `Host` header will be
+	/// checked against the list, connections without a matching host will be denied.
+	/// Values should be just hostnames without protocol.
+	///
+	/// ```rust
+	/// # let mut builder = jsonrpsee_ws_server::WsServerBuilder::default();
+	/// builder.set_allowed_hosts(["example.com"]);
+	/// ```
+	pub fn set_allowed_hosts<Host, List>(mut self, list: List) -> Self
+	where
+		List: IntoIterator<Item = Host>,
+		Host: Into<String>,
+	{
+		self.settings.allowed_hosts = AllowedValue::OneOf(list.into_iter().map(Into::into).collect());
+		self
+	}
+
+	/// Restores the default behavior of allowing connections with `Host` header
+	/// containing any value. This will undo any list set by [`set_allowed_hosts`](Builder::set_allowed_hosts).
+	pub fn allow_all_hosts(mut self) -> Self {
+		self.settings.allowed_hosts = AllowedValue::Any;
 		self
 	}
 
