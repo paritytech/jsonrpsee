f6684d43aa6f3cee095a78eb4498d7c2507ac8a6
Maciej Hirsz <hello@maciej.codes>
2021-09-14 15:23:40 +0200

    RAII claims and some comments

--- utils/src/server/resource_limiting.rs
+++ utils/src/server/resource_limiting.rs
@@ -4,20 +4,28 @@ use std::sync::{
 	Arc,
 };
 
+/// Resource definition
 pub struct Resource {
+	/// Human readable label for a resource, e.g.: "CPU", "Memory"...
 	label: &'static str,
+	/// Max capacity of arbitrary units of the resource
 	capacity: u32,
+	/// Default amount of units running a method costs
 	default: u32,
 }
 
+/// Id referencing a resource for `O(1)` lookups
+#[derive(Debug, Clone, Copy)]
 pub struct ResourceId(u8);
 
-pub struct Resources {
+pub struct ResourceBuilder {
 	table: Vec<Resource>,
 }
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
+	#[error("Invalid resource id: {0:?}")]
+	InvalidResourceId(ResourceId),
 	#[error("Resource at capacity: {0}")]
 	ResourceAtCapacity(&'static str),
 	#[error("Resource name already taken: {0}")]
@@ -26,9 +34,9 @@ pub enum Error {
 	MaxResourcesReached,
 }
 
-impl Resources {
+impl ResourceBuilder {
 	pub fn new() -> Self {
-		Resources { table: Vec::new() }
+		ResourceBuilder { table: Vec::new() }
 	}
 
 	pub fn get(&self, label: &str) -> Option<(ResourceId, &Resource)> {
@@ -51,7 +59,7 @@ impl Resources {
 		Ok(id)
 	}
 
-	pub fn into_internal(self) -> ResourcesInternal {
+	pub fn build(self) -> ResourcesInternal {
 		ResourcesInternal {
 			table: self
 				.table
@@ -63,7 +71,7 @@ impl Resources {
 }
 
 #[derive(Debug)]
-pub struct ResourceInternal {
+struct ResourceInternal {
 	/// We store the label, although we only use it for verbose errors
 	label: &'static str,
 	/// Max capacity of arbitrary units of the resource
@@ -78,13 +86,18 @@ pub struct ResourcesInternal {
 }
 
 impl ResourcesInternal {
-	pub fn get(&self, id: ResourceId) -> Option<&ResourceInternal> {
-		self.table.get(id.0 as usize)
+	/// Attempt to claim a given amount of units of a resource using the `id`.
+	pub fn claim(&self, id: ResourceId, units: u32) -> Result<ClaimedResource, Error> {
+		let resource = self.table.get(id.0 as usize).ok_or_else(|| Error::InvalidResourceId(id))?;
+
+		resource.claim(units)?;
+
+		Ok(ClaimedResource { resource, units })
 	}
 }
 
 impl ResourceInternal {
-	pub fn claim(&self, units: u32) -> Result<(), Error> {
+	fn claim(&self, units: u32) -> Result<(), Error> {
 		let previous = self.used.fetch_add(units, Ordering::SeqCst);
 
 		if previous + units > self.capacity {
@@ -95,12 +108,24 @@ impl ResourceInternal {
 		}
 	}
 
-	pub fn free(&self, units: u32) {
+	fn free(&self, units: u32) {
 		let previous = self.used.fetch_sub(units, Ordering::SeqCst);
 
 		// Overflow below 0 should never happen!
 		if previous < units {
 			panic!("Trying to free more units than where used for resource: {}", self.label);
 		}
+	}
+}
+
+/// RAII style "lock" for claimed resources, will automatically release them once dropped.
+pub struct ClaimedResource<'a> {
+	resource: &'a ResourceInternal,
+	units: u32,
+}
+
+impl Drop for ClaimedResource<'_> {
+	fn drop(&mut self) {
+		self.resource.free(self.units)
 	}
 }
