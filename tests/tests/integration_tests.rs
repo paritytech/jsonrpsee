68d241fc9c4c0fcfb4613f499935b26e01204a89
David Palm <dvdplm@gmail.com>
2021-09-30 10:46:36 +0200

    Un-ignore test that is flaky on windows

--- tests/tests/integration_tests.rs
+++ tests/tests/integration_tests.rs
@@ -297,7 +297,6 @@ async fn server_should_be_able_to_close_subscriptions() {
 }
 
 #[tokio::test]
-#[cfg_attr(target_os = "windows", ignore)]
 async fn ws_close_pending_subscription_when_server_terminated() {
 	let (server_addr, handle) = websocket_server_with_subscription().await;
 	let server_url = format!("ws://{}", server_addr);
