37f77d13c8bab03a6d2787de65b4081f58687aff
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2021-11-18 17:09:30 +0100

    commit added tests

---
+++ proc-macros/tests/ui/incorrect/sub/sub_dup_name_override.rs
@@ -0,0 +1,12 @@
+use jsonrpsee::{proc_macros::rpc, types::RpcResult};
+
+// Subscription method must not use the same override name.
+#[rpc(client, server)]
+pub trait DupOverride {
+	#[subscription(name = "one" => "override", item = u8)]
+	fn one(&self) -> RpcResult<()>;
+	#[subscription(name = "two" => "override", item = u8)]
+	fn two(&self) -> RpcResult<()>;
+}
+
+fn main() {}
