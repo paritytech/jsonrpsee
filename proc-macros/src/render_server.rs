5067f0dbc6770eb84b51cbadbad5191fe967ef49
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2021-11-19 14:53:25 +0100

    Update proc-macros/src/render_server.rs
    Co-authored-by: David <dvdplm@gmail.com>

--- proc-macros/src/render_server.rs
+++ proc-macros/src/render_server.rs
@@ -175,7 +175,7 @@ impl RpcDescription {
 				// Name of the RPC method to subscribe to (e.g. `foo_sub`).
 				let rpc_sub_name = self.rpc_identifier(&sub.name);
 				// When subscribing to an RPC, users can override the content of the `method` field in the JSON data sent to subscribers. Each subscription thus has one method name to set up the subscription, one to unsubscribe and, optionally, a third method name used to describe the payload sent back from the server to subscribers. If no override is provided, the subscription method name is used.
-				let maybe_custom_notif = sub.override_notif_method.as_ref().map(|m| self.rpc_identifier(m));
+				let payload_name = sub.payload_name_override.as_ref().map(|m| self.rpc_identifier(m));
 				// Name of the RPC method to unsubscribe (e.g. `foo_sub`).
 				let rpc_unsub_name = self.rpc_identifier(&sub.unsubscribe);
 				// `parsing` is the code associated with parsing structure from the
