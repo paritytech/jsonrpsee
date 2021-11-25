c44422de6ed1c2fe38d4189b19743768948d80ba
alvicsam <alvicsam@gmail.com>
2021-11-25 15:17:06 +0100

    send 2 metrics with benchmark results

--- .scripts/ci/push_bench_results.sh
+++ .scripts/ci/push_bench_results.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #The script takes output.txt, removes every line that doesn't have "test"
 #in it and pushes benchmark result to Victoria Metrics
-#Benchmark name should have underscores in name (e.g. async/http_concurrent_round_trip/8)
+#Benchmark name should have underscores in the name instead of spaces (e.g. async/http_concurrent_round_trip/8)
 
 RESULT_FILE=$1
 CURRENT_DIR=$(pwd)
@@ -19,6 +19,10 @@ while IFS= read -r line
 do
   BENCH_NAME=$(echo $line | cut -f 2 -d ' ')
   BENCH_RESULT=$(echo $line | cut -f 5 -d ' ')
-  curl -d 'parity_benchmark_result_ns{project="'${CI_PROJECT_NAME}'",metric="'$BENCH_NAME'"} '$BENCH_RESULT'' \
+  #send common result
+  curl -d 'parity_benchmark_common_result_ns{project="'${CI_PROJECT_NAME}'",benchmark="'$BENCH_NAME'"} '$BENCH_RESULT'' \
+    -X POST 'http://vm-longterm.parity-build.parity.io/api/v1/import/prometheus'
+  #send specific result with commit sha
+  curl -d 'parity_benchmark_specific_result_ns{project="'${CI_PROJECT_NAME}'",benchmark="'$BENCH_NAME'",commit="'${CI_COMMIT_SHORT_SHA}'"} '$BENCH_RESULT'' \
     -X POST 'http://vm-longterm.parity-build.parity.io/api/v1/import/prometheus'
 done < "$INPUT"
