f319d58e4b1a065ed0ddd3c7aa2aeb34b2c8e53f
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2021-12-22 14:23:52 +0100

    fix jsonrpsee: feature renamed `client` -> `async-client` (#625)
    * feature renamed to async-client
    
    * export rpc_macro for core client
    
    * re-export core from wrapper crate
    
    * rename feature core-client -> async-client
    
    * add client feature too
    
    * reorg features

--- jsonrpsee/src/lib.rs
+++ jsonrpsee/src/lib.rs
@@ -43,6 +43,9 @@
 //! - **`client`** - Enables `http-client` and `ws-client` features.
 //! - **`server`** - Enables `http-server` and `ws-server` features.
 //! - **`full`** - Enables `client`, `server` and `macros` features.
+//! - **`async-client`** - Enables the async client without any transport.
+//! - **`client-ws-transport`** - Enables `ws` transport with TLS.
+//! - **`client-ws-transport-no-tls`** - Enables `ws` transport without TLS.
 
 /// JSON-RPC HTTP client.
 #[cfg(feature = "jsonrpsee-http-client")]
@@ -56,7 +59,7 @@ pub use jsonrpsee_ws_client as ws_client;
 pub use jsonrpsee_client_transport as client_transport;
 
 /// JSON-RPC client convenience macro to build params.
-#[cfg(any(feature = "http-client", feature = "ws-client"))]
+#[cfg(any(feature = "async-client", feature = "http-client", feature = "ws-client"))]
 pub use jsonrpsee_core::rpc_params;
 
 /// JSON-RPC HTTP server.
@@ -79,7 +82,14 @@ pub use jsonrpsee_types as types;
 #[cfg(any(feature = "http-server", feature = "ws-server"))]
 pub use jsonrpsee_core::server::rpc_module::{RpcModule, SubscriptionSink};
 
-#[cfg(any(feature = "http-server", feature = "ws-server", feature = "core-client"))]
+#[cfg(any(
+	feature = "http-server",
+	feature = "ws-server",
+	feature = "client",
+	feature = "async-client",
+	feature = "http-client",
+	feature = "ws-client"
+))]
 pub use jsonrpsee_core as core;
 
 #[cfg(feature = "http-server")]
