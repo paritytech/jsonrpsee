1f670e156655756cd57dfd202223ba0e28dc830e
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2022-02-18 08:34:53 +0100

    Update proc-macros/tests/ui/correct/alias_doesnt_use_namespace.rs
    Co-authored-by: David <dvdplm@gmail.com>

--- proc-macros/tests/ui/correct/alias_doesnt_use_namespace.rs
+++ proc-macros/tests/ui/correct/alias_doesnt_use_namespace.rs
@@ -2,7 +2,7 @@ use jsonrpsee::{core::RpcResult, proc_macros::rpc};
 
 #[rpc(client, server, namespace = "myapi")]
 pub trait Rpc {
-	/// Aliases doesn't use the namespace.
+	/// Aliases don't use the namespace.
 	/// Thus, this will generate `myapi_getTemp` and `getTemp`.
 	#[method(name = "getTemp", aliases = ["getTemp"])]
 	async fn async_method(&self, param_a: u8, param_b: String) -> RpcResult<u16>;
