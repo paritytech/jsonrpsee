eca3518f10a0f72ecf06fab23f395e31048b335f
Alexandru Vasile <alexandru.vasile@parity.io>
2022-07-11 20:26:54 +0300

    core: Remove `Connection` from common list
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- core/src/server/access_control/cors.rs
+++ core/src/server/access_control/cors.rs
@@ -333,10 +333,12 @@ lazy_static! {
 		hs.insert(Ascii::new("Access-Control-Request-Headers"));
 		hs.insert(Ascii::new("Access-Control-Request-Method"));
 
+		// All requests must have a valid "Host" value that is filtered internally.
 		hs.insert(Ascii::new("Host"));
+		// "Content-Length" is capped by `max_request_body_size` (Default: 10 MB)
 		hs.insert(Ascii::new("Content-Length"));
+		// Set by various tools: (ie `User-Agent: curl/7.79.1`).
 		hs.insert(Ascii::new("Connection"));
-		hs.insert(Ascii::new("User-Agent"));
 		hs
 	};
 }
