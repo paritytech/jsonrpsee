89b03c586c4ec50b69061f88dfa2173acf0f44db
Alexandru Vasile <alexandru.vasile@parity.io>
2022-08-29 16:59:10 +0300

    examples: Re-enable proc-macro example
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- examples/examples/proc_macro.rs
+++ examples/examples/proc_macro.rs
@@ -7,11 +7,11 @@ pub trait Rpc {
 	#[method(name = "foo")]
 	async fn async_method(&self, param_a: u8, param_b: String) -> RpcResult<u16>;
 
-	// #[method(name = "bar")]
-	// fn sync_method(&self) -> RpcResult<u16>;
+	#[method(name = "bar")]
+	fn sync_method(&self) -> RpcResult<u16>;
 
-	// #[subscription(name = "subscribe", item = String)]
-	// fn sub(&self);
+	#[subscription(name = "subscribe", item = String)]
+	fn sub(&self);
 }
 
 fn main() {}
