afbb44bc31f5b5a55dff256fbcaffb1c45beff21
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2022-09-22 22:57:53 +0200

    change to 100s

--- benches/bench.rs
+++ benches/bench.rs
@@ -23,17 +23,17 @@ criterion_group!(
 );
 criterion_group!(
 	name = sync_benches;
-	config = Criterion::default().with_profiler(PProfProfiler::new(100, Output::Flamegraph(None))).measurement_time(Duration::from_secs(60));
+	config = Criterion::default().with_profiler(PProfProfiler::new(100, Output::Flamegraph(None))).measurement_time(Duration::from_secs(100));
 	targets = SyncBencher::http_benches, SyncBencher::websocket_benches
 );
 criterion_group!(
 	name = async_benches;
-	config = Criterion::default().with_profiler(PProfProfiler::new(100, Output::Flamegraph(None))).measurement_time(Duration::from_secs(60));
+	config = Criterion::default().with_profiler(PProfProfiler::new(100, Output::Flamegraph(None))).measurement_time(Duration::from_secs(100));
 	targets = AsyncBencher::http_benches, AsyncBencher::websocket_benches
 );
 criterion_group!(
 	name = subscriptions;
-	config = Criterion::default().with_profiler(PProfProfiler::new(100, Output::Flamegraph(None))).measurement_time(Duration::from_secs(60));
+	config = Criterion::default().with_profiler(PProfProfiler::new(100, Output::Flamegraph(None))).measurement_time(Duration::from_secs(100));
 	targets = AsyncBencher::subscriptions
 );
 criterion_main!(types_benches, sync_benches, async_benches, subscriptions);
