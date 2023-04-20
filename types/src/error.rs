f281c928be5452868c061b288054781e71b31132
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2023-04-20 12:52:45 +0200

    fix error object display impl

--- types/src/error.rs
+++ types/src/error.rs
@@ -39,7 +39,7 @@ pub type ErrorObjectOwned = ErrorObject<'static>;
 /// [Failed JSON-RPC response object](https://www.jsonrpc.org/specification#response_object).
 #[derive(Debug, Deserialize, Serialize, Clone, thiserror::Error)]
 #[serde(deny_unknown_fields)]
-#[error("kkk")]
+#[error("{self:?}")]
 pub struct ErrorObject<'a> {
 	/// Code
 	code: ErrorCode,
