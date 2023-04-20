32c5724ac18e7910c4cddf474b9d7a088de31a0f
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2023-04-20 12:22:30 +0200

    Update core/src/lib.rs

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -62,7 +62,7 @@ pub use async_trait::async_trait;
 pub use error::{Error, GenericTransportError, StringError};
 
 /// JSON-RPC result.
-pub type RpcResult<T> = std::result::Result<T, ErrorObjectOwned>;
+pub type RpcResult<T> = std::result::Result<T, jsonrpee_types::ErrorObjectOwned>;
 
 /// Empty server `RpcParams` type to use while registering modules.
 pub type EmptyServerParams = Vec<()>;
