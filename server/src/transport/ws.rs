600d41ff05a4219202d75c64cd5a2bdb938f8bd1
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2023-10-19 15:23:59 +0200

    graceful shutdown: distinguish between stopped and conn closed (#1220)
    * graceful shutdown: distinguish between stopped and conn closed
    
    * grumbles: ws_stream fuse

--- server/src/transport/ws.rs
+++ server/src/transport/ws.rs
@@ -288,13 +288,15 @@ pub(crate) async fn background_task<L: Logger>(sender: Sender, receiver: Receive
 			// Return the `Closed` error to avoid logging unnecessary warnings on clean shutdown.
 			Err(e) => Some((Err(e), receiver)),
 		}
-	});
+	})
+	.fuse();
 
 	tokio::pin!(ws_stream);
 
 	let result = loop {
 		let data = match try_recv(&mut ws_stream, stopped, ping_config).await {
-			Receive::Shutdown => break Ok(Shutdown::Stopped),
+			Receive::ConnectionClosed => break Ok(Shutdown::ConnectionClosed),
+			Receive::Stopped => break Ok(Shutdown::Stopped),
 			Receive::Ok(data, stop) => {
 				stopped = stop;
 				data
@@ -408,7 +410,8 @@ async fn send_task(
 }
 
 enum Receive<S> {
-	Shutdown,
+	ConnectionClosed,
+	Stopped,
 	Err(SokettoError, S),
 	Ok(Vec<u8>, S),
 }
@@ -428,7 +431,7 @@ where
 	loop {
 		match futures_util::future::select(futs, stopped).await {
 			// The connection is closed.
-			Either::Left((Either::Left((None, _)), _)) => break Receive::Shutdown,
+			Either::Left((Either::Left((None, _)), _)) => break Receive::ConnectionClosed,
 			// The message has been received, we are done
 			Either::Left((Either::Left((Some(Ok(Incoming::Data(d))), _)), s)) => break Receive::Ok(d, s),
 			// Got a pong response, update our "last seen" timestamp.
@@ -457,7 +460,7 @@ where
 				futs = futures_util::future::select(rcv, inactivity_check);
 			}
 			// Server has been stopped.
-			Either::Right(_) => break Receive::Shutdown,
+			Either::Right(_) => break Receive::Stopped,
 		}
 	}
 }
