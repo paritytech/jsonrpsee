e6b1a0f09872133662069475b9b230630e1b688d
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2023-11-22 13:18:52 +0100

    address grumbles

--- tests/tests/integration_tests.rs
+++ tests/tests/integration_tests.rs
@@ -116,8 +116,8 @@ async fn ws_unsubscription_works() {
 
 	sub.unsubscribe().await.unwrap();
 
+	let res = rx.next().with_default_timeout.await.expect("Test must complete with 1 min");
 	// When the subscription is closed a message is sent out on this channel.
-	let res = rx.next().with_timeout(std::time::Duration::from_secs(10)).await.expect("Timeout 10 secs exceeded");
 	assert!(res.is_some());
 }
 
@@ -137,8 +137,8 @@ async fn ws_unsubscription_works_over_proxy_stream() {
 
 	sub.unsubscribe().await.unwrap();
 
+	let res = rx.next().with_default_timeout.await.expect("Test must complete with 1 min");
 	// When the subscription is closed a message is sent out on this channel.
-	let res = rx.next().with_timeout(std::time::Duration::from_secs(10)).await.expect("Timeout 10 secs exceeded");
 	assert!(res.is_some());
 }
 
