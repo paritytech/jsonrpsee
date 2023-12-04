cc85b30b8eafc1d2dbde6118add153278b82d5f9
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2023-12-04 11:23:59 +0100

    Update core/src/client/async_client/mod.rs

--- core/src/client/async_client/mod.rs
+++ core/src/client/async_client/mod.rs
@@ -428,7 +428,7 @@ impl ClientT for Client {
 		R: DeserializeOwned,
 	{
 		// TODO: remove unwrap
-		let batch = batch.build().unwrap();
+		let batch = batch.build()?;
 		let guard = self.id_manager.next_request_id()?;
 		let id_range = generate_batch_id_range(&guard, batch.len() as u64)?;
 
