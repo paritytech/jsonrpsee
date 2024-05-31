a312dcee03b69e4dd25d813ef5c0cd3ac29e55d1
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2024-05-31 14:50:46 +0200

    fix fmt

--- server/src/transport/ws.rs
+++ server/src/transport/ws.rs
@@ -306,7 +306,11 @@ where
 						*missed_pings += 1;
 
 						if *missed_pings >= p.max_failures {
-							tracing::debug!(target: LOG_TARGET, "WS ping/pong inactivity limit reached; closing connection);
+							tracing::debug!(
+								target: LOG_TARGET,
+								"WS ping/pong inactivity limit `{}` exceeded; closing connection"
+								p.max_failures,
+							);
 							break Receive::ConnectionClosed;
 						}
 					}
