f6d3e6a230a8ffdbeb8799e3eb181df46a412fa6
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2024-06-05 17:58:46 +0200

    feat: add macro attr `with_extensions`

--- proc-macros/src/render_server.rs
+++ proc-macros/src/render_server.rs
@@ -237,15 +237,16 @@ impl RpcDescription {
 							})
 						})
 					}
-				} else if sub.with_extensions {
-					handle_register_result(quote! {
+				} else {
+					if sub.with_extensions {
+						handle_register_result(quote! {
 							rpc.register_subscription_raw(#rpc_sub_name, #rpc_notif_name, #rpc_unsub_name, |params, mut pending, context, ext| {
 								#parsing
 								let _ = context.as_ref().#rust_method_name(pending, &ext, #params_seq);
 								#sub_err::None
 							})
 						})
-				} else {
+					} else {
 						handle_register_result(quote! {
 							rpc.register_subscription_raw(#rpc_sub_name, #rpc_notif_name, #rpc_unsub_name, |params, mut pending, context, _| {
 								#parsing
@@ -254,6 +255,7 @@ impl RpcDescription {
 							})
 						})
 					}
+				}
 			})
 			.collect::<Vec<_>>();
 
