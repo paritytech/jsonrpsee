995d754da86f1580497bf92147f4c443a44354f1
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2025-04-21 12:52:33 +0200

    cargo fmt

--- examples/examples/client_subscription_drop_oldest_item.rs
+++ examples/examples/client_subscription_drop_oldest_item.rs
@@ -29,100 +29,100 @@ use std::time::Duration;
 
 use futures::{Stream, StreamExt};
 use jsonrpsee::core::DeserializeOwned;
+use jsonrpsee::core::client::SubscriptionError;
 use jsonrpsee::core::client::{Subscription, SubscriptionClientT};
 use jsonrpsee::rpc_params;
 use jsonrpsee::server::{RpcModule, Server};
 use jsonrpsee::ws_client::WsClientBuilder;
 use tokio_stream::wrappers::BroadcastStream;
 use tokio_stream::wrappers::errors::BroadcastStreamRecvError;
-use jsonrpsee::core::client::SubscriptionError;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    tracing_subscriber::FmtSubscriber::builder()
-        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
-        .try_init()
-        .expect("setting default subscriber failed");
+	tracing_subscriber::FmtSubscriber::builder()
+		.with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+		.try_init()
+		.expect("setting default subscriber failed");
 
-    let addr = run_server().await?;
-    let url = format!("ws://{}", addr);
+	let addr = run_server().await?;
+	let url = format!("ws://{}", addr);
 
-    let client = WsClientBuilder::default().build(&url).await?;
+	let client = WsClientBuilder::default().build(&url).await?;
 
-    let sub: Subscription<i32> = client.subscribe("subscribe_hello", rpc_params![], "unsubscribe_hello").await?;
+	let sub: Subscription<i32> = client.subscribe("subscribe_hello", rpc_params![], "unsubscribe_hello").await?;
 
-    // drop oldest messages from subscription:
-    let mut sub = drop_oldest_when_lagging(sub, 10);
+	// drop oldest messages from subscription:
+	let mut sub = drop_oldest_when_lagging(sub, 10);
 
-    // Simulate that polling takes a long time.
-    tokio::time::sleep(Duration::from_secs(1)).await;
+	// Simulate that polling takes a long time.
+	tokio::time::sleep(Duration::from_secs(1)).await;
 
-    // The subscription starts from zero but you can
-    // notice that many items have been replaced
-    // because the subscription wasn't polled.
-    for _ in 0..10 {
-        match sub.next().await.unwrap() {
-            Ok(n) => {
-                tracing::info!("recv={n}");
-            }
-            Err(e) => {
-                tracing::info!("{e}");
-            }
-        };
-    }
+	// The subscription starts from zero but you can
+	// notice that many items have been replaced
+	// because the subscription wasn't polled.
+	for _ in 0..10 {
+		match sub.next().await.unwrap() {
+			Ok(n) => {
+				tracing::info!("recv={n}");
+			}
+			Err(e) => {
+				tracing::info!("{e}");
+			}
+		};
+	}
 
-    Ok(())
+	Ok(())
 }
 
 fn drop_oldest_when_lagging<T: Clone + DeserializeOwned + Send + Sync + 'static>(
-    mut sub: Subscription<T>,
-    buffer_size: usize,
+	mut sub: Subscription<T>,
+	buffer_size: usize,
 ) -> impl Stream<Item = Result<T, SubscriptionError>> {
-    let (tx, rx) = tokio::sync::broadcast::channel(buffer_size);
+	let (tx, rx) = tokio::sync::broadcast::channel(buffer_size);
 
-    tokio::spawn(async move {
-        // Poll the subscription which ignores errors.
-        while let Some(n) = sub.next().await {
-            let msg = match n {
-                Ok(msg) => msg,
-                Err(e) => {
-                    tracing::error!("Failed to decode the subscription message: {e}");
-                    continue;
-                }
-            };
+	tokio::spawn(async move {
+		// Poll the subscription which ignores errors.
+		while let Some(n) = sub.next().await {
+			let msg = match n {
+				Ok(msg) => msg,
+				Err(e) => {
+					tracing::error!("Failed to decode the subscription message: {e}");
+					continue;
+				}
+			};
 
-            if tx.send(msg).is_err() {
-                return;
-            }
-        }
-    });
+			if tx.send(msg).is_err() {
+				return;
+			}
+		}
+	});
 
-    BroadcastStream::new(rx)
+	BroadcastStream::new(rx)
 }
 
 async fn run_server() -> anyhow::Result<SocketAddr> {
-    let server = Server::builder().build("127.0.0.1:0").await?;
-    let mut module = RpcModule::new(());
-    module
-        .register_subscription("subscribe_hello", "s_hello", "unsubscribe_hello", |_, pending, _, _| async move {
-            let sub = pending.accept().await.unwrap();
+	let server = Server::builder().build("127.0.0.1:0").await?;
+	let mut module = RpcModule::new(());
+	module
+		.register_subscription("subscribe_hello", "s_hello", "unsubscribe_hello", |_, pending, _, _| async move {
+			let sub = pending.accept().await.unwrap();
 
-            for i in 0..usize::MAX {
-                let json = serde_json::value::to_raw_value(&i).unwrap();
-                sub.send(json).await.unwrap();
-                tokio::time::sleep(Duration::from_millis(10)).await;
-            }
+			for i in 0..usize::MAX {
+				let json = serde_json::value::to_raw_value(&i).unwrap();
+				sub.send(json).await.unwrap();
+				tokio::time::sleep(Duration::from_millis(10)).await;
+			}
 
-            Ok(())
-        })
-        .unwrap();
-    let addr = server.local_addr()?;
+			Ok(())
+		})
+		.unwrap();
+	let addr = server.local_addr()?;
 
-    let handle = server.start(module);
+	let handle = server.start(module);
 
-    // In this example we don't care about doing shutdown so let's it run forever.
-    // You may use the `ServerHandle` to shut it down or manage it yourself.
-    tokio::spawn(handle.stopped());
+	// In this example we don't care about doing shutdown so let's it run forever.
+	// You may use the `ServerHandle` to shut it down or manage it yourself.
+	tokio::spawn(handle.stopped());
 
-    Ok(addr)
+	Ok(addr)
 }
